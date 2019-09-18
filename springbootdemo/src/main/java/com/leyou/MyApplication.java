f45995a65cef20daa1af7726931a7e29916fb31e
Mr.Li <ljxfreedom@sina.com>
2019-09-18 23:10:43 +0800

    再次还原

--- springbootdemo/src/main/java/com/leyou/MyApplication.java
+++ springbootdemo/src/main/java/com/leyou/MyApplication.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication // 固定的注解
 public class MyApplication {
     public static void main(String[] args) {
+		
+		System.out.println('张三修改了代码');
 
         SpringApplication.run(MyApplication.class,args);
     }
