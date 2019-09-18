b5179f09feb6b644b249e9a4dbfbc279be42e376
Mr.Li <ljxfreedom@sina.com>
2019-09-18 21:52:17 +0800

    王五修改了代码

--- springbootdemo/src/main/java/com/leyou/MyApplication.java
+++ springbootdemo/src/main/java/com/leyou/MyApplication.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.ComponentScan;
 public class MyApplication {
     public static void main(String[] args) {
 		
-		System.out.println('成功了');
+		System.out.println('王五修改了代码');
 
         SpringApplication.run(MyApplication.class,args);
     }
