ecc79e85e9dcf15edab2ef4868991acd0a8b5e3f
Mr.Li <ljxfreedom@sina.com>
2019-09-18 21:45:10 +0800

    application中输出一句话

--- springbootdemo/src/main/java/com/leyou/MyApplication.java
+++ springbootdemo/src/main/java/com/leyou/MyApplication.java
@@ -7,6 +7,8 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication // 固定的注解
 public class MyApplication {
     public static void main(String[] args) {
+		
+		System.out.println('成功了');
 
         SpringApplication.run(MyApplication.class,args);
     }
